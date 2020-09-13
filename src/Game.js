8af82a8b85a1aee21b4a66dfc5983e052d227b24
PATEL HIT <63950631+hitpatelp007@users.noreply.github.com>
2020-09-13 21:59:09 +0530

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -9,8 +9,7 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          
         </div>
       </div>
     );
